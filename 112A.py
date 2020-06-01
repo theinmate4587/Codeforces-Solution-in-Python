32610fe3138e85e9fa3e3777485ae5a738b5a1aa
Harshal Sinha <harshalsinha437@gmail.com>
2020-06-01 08:09:53 +0530

    Add files via upload

---
+++ 112A.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 09:19:26 2020
+
+@author: Harshal
+"""
+
+
+str1=input().lower()
+str2=input().lower()
+if str1<str2:
+    print('-1')
+elif str1>str2:
+    print("1")
+else:
+    print("0")
